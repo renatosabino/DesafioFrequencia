a1d8fc629347e332e92d2c935eaebd16bbc2880c
Renato Sabino <resabino1996@gmail.com>
2023-12-15 22:30:38 -0300

    feat: inclusão de controller de desafios.

---
+++ DesafioFrequencia.Api/Controllers/DesafioController.cs
@@ -0,0 +1,57 @@
+﻿using DesafioFrequencia.Application.Desafios.AlterarNome;
+using DesafioFrequencia.Application.Desafios.AlterarPeriodo;
+using DesafioFrequencia.Application.Desafios.AlterarRegra;
+using DesafioFrequencia.Application.Desafios.Criar;
+using DesafioFrequencia.Application.Desafios.IncluirParticipante;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioFrequencia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DesafioController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public DesafioController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("Criar")]
+        public async Task<IActionResult> Criar([FromBody] CriarCommand criar, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(criar, cancellationToken);
+            return Ok();
+        }
+
+        [HttpPut("AlterarNome")]
+        public async Task<IActionResult> AlterarNome([FromBody] AlterarNomeCommand alterarNome, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(alterarNome, cancellationToken);
+            return Ok();
+        }
+
+        [HttpPut("AlterarRegra")]
+        public async Task<IActionResult> AlterarRegra([FromBody] AlterarRegraCommand alterarRegra, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(alterarRegra, cancellationToken);
+            return Ok();
+        }
+
+        [HttpPut("AlterarPeriodo")]
+        public async Task<IActionResult> AlterarPeriodo([FromBody] AlterarPeriodoCommand alterarPeriodo, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(alterarPeriodo, cancellationToken);
+            return Ok();
+        }
+
+        [HttpPost("IncluirParticipante")]
+        public async Task<IActionResult> IncluirParticipante([FromBody] IncluirParticipanteCommand incluirParticipante, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(incluirParticipante, cancellationToken);
+            return Ok();
+        }
+    }
+}
