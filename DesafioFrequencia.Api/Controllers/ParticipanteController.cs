93194288a44e7641a492bc42edf7f9fccc37b949
Renato Sabino <resabino1996@gmail.com>
2023-12-15 22:39:49 -0300

    feat: criação de controller de participante.

---
+++ DesafioFrequencia.Api/Controllers/ParticipanteController.cs
@@ -0,0 +1,52 @@
+﻿using DesafioFrequencia.Application.Participantes.AlterarImagem;
+using DesafioFrequencia.Application.Participantes.Editar;
+using DesafioFrequencia.Application.Participantes.Registrar;
+using DesafioFrequencia.Infra.Utils.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioFrequencia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParticipanteController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly IImageUploadService _imageUploadService;
+        private readonly IConfiguration _configuration;
+
+        public ParticipanteController(IMediator mediator, IImageUploadService imageUploadService, IConfiguration configuration)
+        {
+            _mediator = mediator;
+            _imageUploadService = imageUploadService;
+            _configuration = configuration;
+        }
+
+        [HttpPost("Registrar")]
+        public async Task<IActionResult> Registrar([FromBody] RegistrarCommand registrar, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(registrar, cancellationToken);
+            return Ok();
+        }
+
+        [HttpPut("Editar")]
+        public async Task<IActionResult> Editar([FromBody] EditarCommand editar, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(editar, cancellationToken);
+            return Ok();
+        }
+
+        [HttpPut("AlterarImagem")]
+        public async Task<IActionResult> AlterarImagem([FromForm] AlterarImagemDTO alterarImagem, CancellationToken cancellationToken)
+        {
+
+            var folder = _configuration["ImageUploadFolders:ParticipanteFolder"];
+            var imagem = await _imageUploadService.UploadImageAsync(alterarImagem.Imagem, folder);
+
+            var alterarImagemCommand = new AlterarImagemCommand(alterarImagem.Id, imagem);
+
+            await _mediator.Send(alterarImagemCommand, cancellationToken);
+            return Ok();
+        }
+    }
+}
---
+++ DesafioFrequencia.Application/Participantes/AlterarImagem/AlterarImagemDTO.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace DesafioFrequencia.Application.Participantes.AlterarImagem;
+public sealed record AlterarImagemDTO(int Id, IFormFile Imagem);
+
